4f9d4802692cb4f3ad31c20a8daa49d3ae09c0a9
ChristianOT <c_oual01@wwu.de>
2016-09-28 10:18:11 +0200

    Upload code for interaction with yoink

---
+++ src/main/java/yoinkClasses/MSAdaptiveProcessor.java
@@ -0,0 +1,8 @@
+//import org.wallerlab.yoink.api.model.bootstrap.Job;
+//import org.wallerlab.yoink.api.model.molecular.MolecularSystem;
+//
+//interface MSAdaptiveProcessor {
+//	
+//	public Job process(MolecularSystem MS,String fileName);
+//
+//}
